c82b80ffd81518428de69a65c7b4a5f82d0cbc2b
viniciussoares7 <vinicius-a-soares@outlook.com>
2021-07-15 16:40:50 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+/* IMAGES carousel slider swiper */
+const swiper = new Swiper('.swiper-container', {
+  slidesPerView: 1,
+  pagination: {
+    el: '.swiper-pagination'
+  },
+  mousewheel: true,
+  keyboard: true,
+  breakpoints: {
+    767: {
+      slidesPerView: 2,
+      setWrapperSize: true
+    }
+  }
+})
