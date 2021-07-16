720beed08d3998a86dad3a9ae1d7e6562cebeabc
viniciussoares7 <vinicius-a-soares@outlook.com>
2021-07-15 22:35:51 -0300

    done for desktops

--- script.js
+++ script.js
@@ -2,10 +2,16 @@
 const swiper = new Swiper('.swiper-container', {
   slidesPerView: 1,
   pagination: {
-    el: '.swiper-pagination'
+    el: '.swiper-pagination',
+    type: 'progressbar'
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev'
   },
   mousewheel: true,
   keyboard: true,
+
   breakpoints: {
     767: {
       slidesPerView: 2,
@@ -13,3 +19,35 @@ const swiper = new Swiper('.swiper-container', {
     }
   }
 })
+
+const form = document.getElementById('form')
+
+form.addEventListener('submit', e => {
+  e.preventDefault()
+  let nome = document.getElementById('nome').value
+  let email = document.getElementById('email').value
+  let telefone = document.getElementById('telefone').value
+  let data = {
+    nome,
+    email,
+    telefone
+  }
+  let convertData = JSON.stringify(data)
+
+  localStorage.setItem('lead', convertData)
+
+  let content = document.getElementById('content')
+
+  let carregando = `<p>carregando...</p>`
+
+  let pronto = `<p id="pronto">Formulario enviado!</p>`
+
+  content.innerHTML = carregando
+
+  setTimeout(() => {
+    content.innerHTML = pronto
+  }, 500)
+})
+
+meuStorage = localStorage
+console.log(meuStorage)
